2102204f4be570801ced863c80874c2229ce82ee
Cameron <camjohnson0302@gmail.com>
2014-04-11 13:33:26 -0500

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,17 +4,22 @@ class HomeController < ApplicationController
 
   def test
   	@neo=Neography::Rest.new
-  	node1 = @neo.create_node("age" => 31, "name" => "Max")
-  	node2 = @neo.create_node("age" => 31, "name" => "Sam")
-  	node3 = @neo.create_node("age" => 31, "name" => "Dan")
-  	@neo.create_relationship(:friends, node1, node2)
-  	@neo.create_relationship(:friends, node2, node3)  
-  	a = @neo.execute_query("START user=node(1) MATCH user<-[:friends]->person<-[:friends]->person2 RETURN person2")
   	
-# MATCH user-[:likes]->Movie<-[:likes]-person-[:likes]->stuff
-# WHERE NOT user-[:likes]->stuff
-# RETURN stuff   
-  	@test = a
-  	@test = @neo.get_node_relationships(node1)   
+  	
+		@node= @neo.execute_query("match (user{ name:'Me'}) return user")
+  	
+  	@me = @neo.get_node(8)['data']['name']
+
+  	# @test = @neo.execute_query("start n=node({id}) MATCH user-[:like]->movie<-[:like]-person2-[:like]->movie2 return movie2", {:id => 8})
+  	
+		# @test = @neo.get_node_properties(@node)
+	
+		  
+	 
+		@result = @neo.execute_query('MATCH (person { name:"Me" })--(movie)--(person2)--(movie2)--(person3)--(movie3) WHERE NOT person--(movie3) RETURN movie2.name,movie3.name')
+		
+		@test = @result['data']
+
+
   end
 end
