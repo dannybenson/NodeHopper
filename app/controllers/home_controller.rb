0aaad6ce5d1d0505bdefc79becc906a8733ad9c0
Cameron <camjohnson0302@gmail.com>
2014-04-11 10:09:48 -0500

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def test
+  	@neo=Neography::Rest.new
+  	node1 = @neo.create_node("age" => 31, "name" => "Max")
+  	node2 = @neo.create_node("age" => 31, "name" => "Sam")
+  	node3 = @neo.create_node("age" => 31, "name" => "Dan")
+  	@neo.create_relationship(:friends, node1, node2)
+  	@neo.create_relationship(:friends, node2, node3)  
+  	a = @neo.execute_query("START user=node(1) MATCH user<-[:friends]->person<-[:friends]->person2 RETURN person2")
+  	
+# MATCH user-[:likes]->Movie<-[:likes]-person-[:likes]->stuff
+# WHERE NOT user-[:likes]->stuff
+# RETURN stuff   
+  	@test = a
+  	@test = @neo.get_node_relationships(node1)   
+  end
 end
