ff677a28ed6d0f15416f4a5fcd73d9f5b4d6ee50
Cameron <camjohnson0302@gmail.com>
2014-04-11 10:09:48 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,18 @@ TeamKen::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+
   root 'home#index'
   get '/template' => 'home#show'
+
   get '/typeahead' => 'home#typeahead'
+
+
+
+
+  
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
