f579f46d555fe5f1e18e8068b77acf8404be7214
Apprentice <apprentice@dbc-chicago-5.local>
2014-04-12 13:45:43 -0500

    bootstrap playground

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 TeamKen::Application.routes.draw do
-  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-
+  get '/template' => 'home#show'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
