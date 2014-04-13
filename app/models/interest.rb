a95f28a2d99be5584143f526932058f2c54c34c1
Apprentice <apprentice@dbc-chicago-5.local>
2014-04-13 14:22:21 -0500

    refactors seed file

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -4,4 +4,16 @@ class Interest
     @neo = ClientHelper.get_client
     @neo.get_nodes_labeled(label).map{ |labeled|  @neo.get_node_properties(labeled, 'name')["name"] }
   end
+
+  def self.find_or_create_by(label, name)
+    @neo = ClientHelper.get_client
+    if @neo.find_nodes_labeled(label, {:name => name}).empty?
+      node = @neo.create_node("name" => name)
+      @neo.add_label(node, ["Interest", label])
+      node
+    else
+      @neo.find_nodes_labeled(label, {:name => name}).first
+    end
+  end
+
 end
