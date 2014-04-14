f06e629861408967cf506edbdd9bf03f3d33af89
Cameron <camjohnson0302@gmail.com>
2014-04-13 21:21:15 -0500

    better pull request

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,50 +1,15 @@
-require 'JSON'
+require 'json'
 
 class HomeController < ApplicationController
+
   def index
-    @neo=Neography::Rest.new
-
-    @result = @neo.execute_query('MATCH (movie { name:"Daft Punk" })--(person)--(recommendation) WHERE NOT movie=recommendation RETURN recommendation.name')
-
-    @titles = @result['data'].map{|d| d[0]}
-    result_array = []
-    @test = @titles.uniq.each do |m|
-      # freq =
-      result_array << {label: m, value: @titles.count(m)}
-    end
-    @data = JSON.generate(result_array.sort{ |a,b| b[:value] <=> a[:value]}[0..9])
   end
-
 
   def template
   end
 
   def typeahead
     render json: Interest.get_interest_names
-
   end
-
-
-  def test
-
-  	@neo=Neography::Rest.new
-    cat = 'fleep'
-    query = 'MATCH (interest {name:{'+cat+'}) RETURN interest.name'
-    @test = query
-
-  	
-
-
- 
-  	@test = a
-  	@test = @neo.get_node_relationships(node1)   
-  	
-		@node= @neo.execute_query("match (user{ name:'Me'}) return user")
-  	
-  	@me = @neo.get_node(8)['data']['name']
-
-
-  end
-
 end
-# [{label: "title", value: <frequency>},]
+
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,10 +3,9 @@ require 'spec_helper'
 describe HomeController do
 
   describe "GET 'index'" do
-    xit "returns http success" do
+    it "returns http success" do
       get 'index'
       response.should be_success
     end
   end
-
 end
