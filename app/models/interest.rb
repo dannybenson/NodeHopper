f13af9ff6e0686a6f229a1d94c565307cfc52451
Cameron <camjohnson0302@gmail.com>
2014-04-13 21:06:50 -0500

    adds ORM and donut support

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -3,87 +3,167 @@ require'neography'
 require'neo4j-cypher'
 class Interest
 
-
+	@@neo = ClientHelper.get_client
 
   def self.get_interest_names(label = "Interest")
-    @neo = ClientHelper.get_client
-    @neo.get_nodes_labeled(label).map{ |labeled|  @neo.get_node_properties(labeled, 'name') }
+    @@neo = ClientHelper.get_client
+    @@neo.get_nodes_labeled(label).map{ |labeled|  @@neo.get_node_properties(labeled, 'name')}
   end
 
   def self.find_or_create_by(label, name)
-    @neo = ClientHelper.get_client
-    if @neo.find_nodes_labeled(label, {:name => name}).empty?
-      node = @neo.create_node("name" => name)
-      @neo.add_label(node, ["Interest", label])
+    @@neo = ClientHelper.get_client
+    if @@neo.find_nodes_labeled(label, {:name => name}).empty?
+      node = @@neo.create_node("name" => name)
+      @@neo.add_label(node, ["Interest", label])
       node
     else
-      @neo.find_nodes_labeled(label, {:name => name}).first
+      @@neo.find_nodes_labeled(label, {:name => name}).first
     end
   end
 
 
 
 
-	@@neo = ClientHelper.get_client
+	
 
-	@@node_count = 0 #?
 	attr_reader :id,:name,:category
 
 	def initialize(args)
-		if args.fetch(:id)
-			@id = args.fetch(:id)
-		else
-			@id||= count
-			@@count+=1
-		end
 		@category = args.fetch(:category)
 		@name = args.fetch(:name)
 	end	
 
 	def save
-		@neo.create_node("id" => self.id, "name" => self.name,"category" => self.category) unless self.in_database?
+		interest = @@neo.create_node("name" => self.name) unless self.in_database?
+		@@neo.add_label(interest, "Interest")
+		@@neo.add_label(interest, self.category)
+		self
 	end
 
-	def self.create(args)
-		node = self.new(args)
-		node.save		
-	end
+	# def self.create(args)
+	# 	node = self.new(args)
+	# 	node.save		
+	# end
 
 	def destroy
-		node = @neo.get_node(self.id) 
-		@neo.delete_node!(node)
+		nodes = @@neo.find_nodes_labeled(self.category, {:name => self.name})
+		nodes.each do |node|
+			@@neo.delete_node!(node)
+		end
 		self		  
 	end
 
-	def << 
-
+	def self.find(label,name)
+		node = @@neo.find_nodes_labeled(label, {:name => name}).first
+		if node
+			name = @@neo.get_node_properties(node)["name"]
+			category = label
+			Interest.new({name:name,category:category})
+		end
 	end
 
+	# def << 
+
+	# end
+
 	def weighted_recommendations(number)
-		recommendations = self.recommendations['data'].map{|d| d['name']}
+		recommendations = self.recommendations
     results = []
-    recommendations.uniq.each do |title|
-      results << {label: title, value: recommendations.count(title)}
+    # titles = recommendations.map{|title| title[1]}
+    unique = recommendations.uniq
+    unique.each do |title|
+      title << recommendations.count{|interest| interest[1] == title[1]}
+      results << title
     end
-    JSON.generate(results.sort{ |a,b| b[:value] <=> a[:value]}[0..number])
+    results.sort{ |a,b| b[2] <=> a[2]}[0..number]
+  end
+
+  def donut(number)
+  	results = {'title' => 'params', 'children' => []}
+  	input = self.weighted_recommendations(number)
+  	categories = input.map(&:first).uniq
+  	categories.each do |category|
+  		category_recs = input.select {|recommendation| recommendation.first == category }
+  		children = []
+  		category_recs.each do |c, title, freq|
+  			children << {"title" => title, "data" => freq}
+  		end
+  		results['children'] << {'title' => category, 'children' => children}  
+  	end
+  	results
   end
 
 
 
 	def recommendations  
-		@neo.execute_query('MATCH (movie {name})--(person)--(recommendation) WHERE NOT movie=recommendation RETURN recommendation.name',{:name => self.name})['data']
+		result = @@neo.execute_query("MATCH (interest {name:'"+ self.name+"'})--(person)--(recommendation) WHERE NOT interest=recommendation RETURN labels(recommendation)[1],recommendation.name")['data']
 	end
 
 	def in_database?
-		@neo = ClientHelper.get_client
 		query = "MATCH (interest {name:'"+self.name+"'}) RETURN interest.name"	
-		if @neo.execute_query(query)['data'][0]
+		if @@neo.find_nodes_labeled(self.category,{:name => self.name}).first
 			true
 		else
 			false
 		end
 	end
-
 end
+
+
+# def weighted_recommendations(number)
+# 	recommendations = self.recommendations
+# 	output = {"title": "params",
+#   "children": []}
+
+#   children = 
+# end
+
+
+# {
+#   "title": "params",
+#   "children": [
+#   {
+#     "title": "Movies",
+#     "children":[
+#     {"title":"Departed",
+#       "data": 30},
+#     {"title":"The Shining",
+#       "data": 70},
+#     {"title":"The Help",
+#       "data": 90},
+#     {"title":"Tomoe",
+#       "data": 50}
+#     ]
+#   },
+#   {
+#     "title": "Books",
+#     "children":[
+#     {"title":"1984",
+#       "data": 30},
+#     {"title":"Mockingjay",
+#       "data": 69},
+#     {"title":"Brave New World",
+#       "data": 23}
+#     ]
+#   },
+#   {
+#     "title": "TV shows",
+#     "children":[
+#     {"title":"OOT",
+#       "data": 78},
+#     {"title":"Something Good",
+#       "data": 20},
+#     {"title":"Cosmos",
+#       "data": 92}
+#     ]
+#   }
+
+
+
+
+
+
+
+
 
 
--- spec/models/interest_spec.rb
+++ spec/models/interest_spec.rb
@@ -1,47 +1,48 @@
 require 'spec_helper'
 require 'neography'
 require 'neo4j-cypher'
-@neo = ClientHelper.get_client
-p @neo.execute_query('MATCH n return n')
+# @@neo = ClientHelper.get_client
+
 describe Interest do
-  describe "new" do
+  describe "self.new" do
 
   	it "creates a new object" do
   		@neo = ClientHelper.get_client
-  		a = Interest.new({id:1,name:"poops",category:'thing'})
+  		a = Interest.new({name:"poops",category:'thing'})
   		expect(a.name).to eq('poops')
   	end
   end
   describe "save" do
-  	xit "creates a node" do
+  	it "creates a node" do
   		@neo = ClientHelper.get_client
-  		a = Interest.new({id:1,name:"sneep",category:'thing'})
+  		a = Interest.new({name:"sneep",category:'thing'})
   		expect(a.in_database?).to eq(false)
   		a.save
   		expect(a.in_database?).to eq(true)
+      a.destroy
   	end
   end  
 
   describe "in_database?" do
   	it "returns true if the interest is in the database" do
   		@neo = ClientHelper.get_client
-  		a = Interest.new({id:1,name:"Batman Begins",category:'thing'})
+  		a = Interest.new({name:"Batman Begins",category:'Movie'})
   		b = a.in_database?
   		expect(b).to eq(true)
   	end 
 
   	it "returns false if the interest is not in the database" do
   		@neo = ClientHelper.get_client
-  		a = Interest.new({id:1,name:"fleeps",category:'thing'})
+  		a = Interest.new({name:"fleeps",category:'thing'})
   		b = a.in_database?
   		expect(b).to eq(false)
   	end
   end
 
   describe "destroy" do
-  	xit "deletes a node from the database" do
+  	it "deletes a node from the database" do
   		@neo = ClientHelper.get_client
-  		a = Interest.new({id:1,name:"flerp flerp",category:'thing'})
+  		a = Interest.new({name:"flimflam",category:'thing'})
   		a.save
   		expect(a.in_database?).to eq(true)
   		a.destroy
@@ -49,12 +50,13 @@ describe Interest do
   	end
   end
 
-
-
-
-
-
-
+  describe "self.find" do
+    it "returns the node searched for" do
+      a = Interest.new({name:"Batman Begins",category:'Movie'})
+      b = Interest.find("Movie","Batman Begins")
+      expect(b.name).to eq(a.name)
+    end
+  end
 end
 
 
