2ea5ea7699dd79cffeffd29bfc5b098ad909d5b9
dannybenson <dan.s.benson@gmail.com>
2014-04-15 14:23:58 -0500

    fix merge seeds

--- db/parser.rb
+++ db/parser.rb
@@ -18,20 +18,6 @@ friends.each do |friend|
   end
 end
 
-p user_movies
-binding.pry
-p
-
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'JSON'
-
 # require 'pry'
 
 
